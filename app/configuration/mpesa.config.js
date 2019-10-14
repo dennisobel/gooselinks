1fda88c76b256dcbe6ffbaa5af7f6b99525bd433
dennisobel <ogembodenis2016@gmail.com>
2019-10-14 15:42:59 +0300

    mpesa configuration changes

--- app/configuration/mpesa.config.js
+++ app/configuration/mpesa.config.js
@@ -4,8 +4,8 @@ module.exports = {
     'ONLINE_SHORT_CODE':"174379",
     'ONLINE_PASS_KEY':"bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919",
     'ACCOUNT_REF':"895421",
-    // 'CALLBACK_URL':"https://edwinmoimbo.herokuapp.com/hooks/mpesa",
-    'CALLBACK_URL':"http://51d3f95c.ngrok.io/hooks/mpesa",
+    'CALLBACK_URL':"https://edwinmoimbo.herokuapp.com/hooks/mpesa",
+    // 'CALLBACK_URL':"http://51d3f95c.ngrok.io/hooks/mpesa",
     'GEN_AUTH_URL':"https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
     'STK_PUSH_URL':"https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
 }
